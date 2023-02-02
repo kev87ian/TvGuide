1825957d7f211669720c19d804a18d767ef351d3
kev87ian <kelvinian87@gmail.com>
2023-02-02 03:32:07 +0300

    dependencies/initial project setup complete

--- app/src/main/java/com/kev/tvguide/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.kev.tvguide.utils
-
-class Constants {
-
-	companion object{
-		const val BASE_URL = "https://api.themoviedb.org/3/"
-		const val BASE_POSTER_URL = "https://image.tmdb.org/t/p/w500/"
-	}
-}
